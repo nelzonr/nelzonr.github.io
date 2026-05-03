e3da1e38c1d4aaaf7f5991c8c6087c93b3cfa7b7
Nelzon <nkramirez@hotmail.com>
2026-05-03 02:43:08 -0300

    feat: adicionar bookmarklets para controle de idiomas e velocidade de áudio, e atualizar scripts de minificação

---
+++ custom_bar_bookmarklet.js
@@ -0,0 +1,10 @@
+javascript:(function () {
+    window.AG_LANGUAGES = [
+        { label: 'PT', locale: 'pt', region: 'r5', lp: 'lp-t' },
+        { label: 'ES', locale: 'es', region: 'r4', lp: 'lp-s' },
+        { label: 'EN', locale: 'en', region: 'r1', lp: 'lp-e' },
+    ];
+    var s = document.createElement('script');
+    s.src = 'https://nelzonr.github.io/custom_bar.min.js';
+    document.head.appendChild(s);
+})();
